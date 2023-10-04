59292346cf8a69d9e06f878b23137863b59d1281
BaselessFabric <alexanderwalls92@gmail.com>
2023-10-04 17:45:18 +0100

    altered styling to fix page width

---
+++ src/app/rootReducer.ts
@@ -0,0 +1,6 @@
+// @ts-nocheck
+import { combineReducers } from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({});
+
+export default rootReducer;
---
+++ src/app/store.ts
@@ -0,0 +1,5 @@
+// @ts-nocheck
+
+import { configureStore, Action } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import { ThunkAction } from "@reduxjs/toolkit";
